9d54ed4850f55623381fcc7345992450ee963cd9
zhangweixiang <weixiang.zhang@xgo.one>
2024-05-29 15:41:51 +0800

    test

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -40,7 +40,6 @@ export default function NavLinks() {
           >
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
-            <p></p>
           </Link>
         );
       })}
